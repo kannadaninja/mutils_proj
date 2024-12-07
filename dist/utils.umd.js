8c4c7e15b7cc8236fefb03bd073ea719ba803bfd
kannadaninja <kannadaninja@gmail.com>
2024-12-07 11:35:54 +0530

    push lib

---
+++ dist/utils.umd.js
@@ -0,0 +1,1 @@
+(function(e,t){typeof exports=="object"&&typeof module<"u"?t(exports):typeof define=="function"&&define.amd?define(["exports"],t):(e=typeof globalThis<"u"?globalThis:e||self,t(e.Utils={}))})(this,function(e){"use strict";const t=(n,d)=>n+d,i=(n,d)=>n-d;e.add=t,e.subtract=i,Object.defineProperty(e,Symbol.toStringTag,{value:"Module"})});
